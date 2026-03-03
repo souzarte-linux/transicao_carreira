8d5959435bebe241554851bf6a91f93cd5c4192d
Fernando Souza <souzaesouzaassociados@gmail.com>
2026-03-03 04:17:45 -0300

    exercicio_GPT/controle_entregas.py proposto pelo GPT

---
+++ exercicio_GPT/controle_entregas.py
@@ -0,0 +1,7 @@
+fez_entrega = True
+quant_pacos = input("Quantos pacotes foram entregues? ")
+valor_diaria = 290
+
+
+
+print("O valor do seu dia foi R$ ", valor_diaria,  " e você entregou ", quant_pacos, ", pacotes.". "Isso quer dizer que cada")
